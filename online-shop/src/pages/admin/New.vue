d1e7facaa3c900599373e6b225799c14a3db6c9f
haidi20 <nhaidii@yahoo.com>
2018-06-29 21:35:21 +0800

    membuat tampilan view product, new product, dan form di new product

--- online-shop/src/pages/admin/New.vue
+++ online-shop/src/pages/admin/New.vue
@@ -49,7 +49,11 @@ export default {
   data(){
     return{
       model: {
-        name: ''
+        name: '',
+        price: '',
+        manufacturer: '',
+        image: '',
+        description: '',
       }
     }
   },
@@ -64,7 +68,7 @@ export default {
 
 <style>
   .form-control.error{
-    border-color: #FF3333;
+    border-color: red;
     box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(255,71,71,0.6);
   }
 </style>
